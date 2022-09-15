474f3e38632d6b69a2b9f71b6871c2567632cdae
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-09-15 20:14:14 +0300

    2.1. Преобразование даты

--- src/main/java/ru.job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru.job4j/grabber/HabrCareerParse.java
@@ -5,8 +5,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class HabrCareerParse {
 
@@ -21,9 +23,11 @@ public class HabrCareerParse {
         rows.forEach(row -> {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
-            String date = row.select(".vacancy-card__date").first().child(0).attr("datetime");
             String vacancyName = titleElement.text();
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            LocalDateTime date = new HabrCareerDateTimeParser().parse(
+                    row.select(".vacancy-card__date").first().child(0).attr("datetime")
+            );
             System.out.printf("%s %s %s%n", vacancyName, link, date);
         });
     }
